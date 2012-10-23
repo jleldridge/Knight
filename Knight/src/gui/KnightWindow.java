399512a71a8e3377bc89bf00bebc955f2b8a1bea
Luke Eldridge <jleldrid@uncg.edu>
2012-10-22 23:14:49 -0400

    Initial commit, just creating basic classes, nothing substantial done yet.

---
+++ Knight/src/gui/KnightWindow.java
@@ -0,0 +1,12 @@
+package gui;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class KnightWindow extends JFrame{
+	public KnightWindow(){
+		
+	}
+}
