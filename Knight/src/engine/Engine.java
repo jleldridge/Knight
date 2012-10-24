c247c0713693b0c9ecc9c527579720c65b5bf8fe
Luke Eldridge <jleldrid@uncg.edu>
2012-10-24 09:45:52 -0400

    Added code to set the player's horizontal speed to the Engine's keyPressed() and keyReleased() methods, as well as code to move the player based on its horizontal speed to the Engine's update() method.

--- Knight/src/engine/Engine.java
+++ Knight/src/engine/Engine.java
@@ -13,15 +13,36 @@ public class Engine {
 	}
 	
 	public void keyPressed(KeyEvent e){
+		//figure out which key is pressed
+		int code = e.getKeyCode();
 		
+		//modify the player's state based on which key was pressed
+		if(code == KeyEvent.VK_LEFT){
+			player.setDx(-10);
+		}
+		else if(code == KeyEvent.VK_RIGHT){
+			player.setDx(10);
+		}
 	}
 	
 	public void keyReleased(KeyEvent e){
+		//figure out which key is released
+		int code = e.getKeyCode();
 		
+		//modify the player's state based on which key was released
+		//in this case only drop speed to zero if the key relevant
+		//to the current dx was released.
+		if(code == KeyEvent.VK_LEFT && player.getDx() < 0){
+			player.setDx(0);
+		}
+		else if(code == KeyEvent.VK_RIGHT && player.getDx() > 0){
+			player.setDx(0);
+		}
 	}
 	
 	public void update(){
-		
+		//change the horizontal position of the player based on their current speed.
+		player.setX(player.getX() + player.getDx());
 	}
 	
 	public void render(Graphics g){
