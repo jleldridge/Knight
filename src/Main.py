09356959b7bf4b0f9cd779b6acae64a22d93d545
jleldridge <jleldridge27@gmail.com>
2013-05-29 17:00:46 -0400

    Added the player class.

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,44 @@
-def main():
-    pass
+import pygame, sys
+import Player
+from pygame.locals import *
 
-if __name__ == __main__:
+def main():
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+    
+    BLACK = (0, 0, 0)
+    
+    player = Player.Player()
+    player.rect.x = 800/2
+    player.rect.y = 600/2
+    
+    # set up the game
+    pygame.init()
+    main_clock = pygame.time.Clock()
+    
+    # create the game window
+    main_window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), 0, 32)
+    pygame.display.set_caption("Knight")
+    
+    # main loop
+    while True:
+        # handle events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        # clear the screen
+        main_window.fill(BLACK)
+        
+        main_window.blit(player.image, player.rect)
+        
+        pygame.display.update()
+        main_clock.tick(60)
+
+if __name__ == '__main__':
     try:
         main()
     except:
         raise
+        
---
+++ src/Player.py
@@ -0,0 +1,14 @@
+import pygame
+from pygame.locals import *
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        
+        # temporarily make the player a white rectangle image
+        self.image = pygame.Surface([20, 50])
+        self.image.fill((255, 255, 255))
+        
+        # get a bounding rectangle of the image
+        self.rect = self.image.get_rect()
