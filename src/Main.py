9bd43578be9bc68406853a7e36a5553d0957bf4e
jleldridge <jleldridge27@gmail.com>
2013-05-29 17:27:25 -0400

    Extracted some code into separate functions from main().

--- src/Main.py
+++ src/Main.py
@@ -2,15 +2,46 @@ import pygame, sys
 import Player
 from pygame.locals import *
 
+def handle_events(events):
+    for event in events:
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_LEFT or event.key == ord('a'):
+                pass
+            if event.key == K_RIGHT or event.key == ord('d'):
+                pass
+            if event.key == K_UP or event.key == ord('w'):
+                pass
+            if event.key == K_DOWN or event.key == ord('s'):
+                pass
+        if event.type == KEYUP:
+            if event.key == K_LEFT or event.key == ord('a'):
+                pass
+            if event.key == K_RIGHT or event.key == ord('d'):
+                pass
+            if event.key == K_UP or event.key == ord('w'):
+                pass
+            if event.key == K_DOWN or event.key == ord('s'):
+                pass
+
+def draw(main_window, objects):
+    BLACK = (0, 0, 0)
+    main_window.fill(BLACK)
+    for o in objects:
+        main_window.blit(o.image, o.rect)
+
 def main():
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
     
-    BLACK = (0, 0, 0)
+    game_objects = []
     
     player = Player.Player()
     player.rect.x = 800/2
     player.rect.y = 600/2
+    game_objects.append(player)
     
     # set up the game
     pygame.init()
@@ -23,15 +54,10 @@ def main():
     # main loop
     while True:
         # handle events
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+        handle_events(pygame.event.get())
         
-        # clear the screen
-        main_window.fill(BLACK)
-        
-        main_window.blit(player.image, player.rect)
+        # draw game objects
+        draw(main_window, game_objects)
         
         pygame.display.update()
         main_clock.tick(60)
