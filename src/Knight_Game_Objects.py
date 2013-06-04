af939eeb97e4226e651f79171eaebcc9f0fa801e
Luke Eldridge <jleldrid@uncg.edu>
2013-06-03 21:29:20 -0400

    Added game objects, need to figure out how to draw them.

---
+++ src/Knight_Game_Objects.py
@@ -0,0 +1,21 @@
+import Tilesets
+
+class Knight_Game_Object:
+
+    def __init__(self, image, solid=False, collide_rect = None):
+        self.image = image
+        self.rect = rect
+        self.solid = solid
+        if self.solid:
+            self.collide_rect = collide_rect
+
+# definitions of game objects
+
+# Pyramid of crates
+image = Pygame.Surface((64, 64))
+image.blit(Tilesets.castle[231], (0, 0))
+image.blit(Tilesets.castle[232], (32, 0))
+image.blit(Tilesets.castle[247], (0, 32))
+image.blit(Tilesets.castle[248], (32, 32))
+rect = image.get_rect()
+pyramid_of_crates = Knight_Game_Object(image, True, rect)
